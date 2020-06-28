2833b3726492ab5d569c668c24e33b330b405ef5
Gyanendra Mishra <anomaly.the@gmail.com>
2020-06-28 22:37:52 +0400

    get existing unit tests to work again

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,27 +1,29 @@
 import unittest
-from link.user import UserTokens
+from link.models.user_tokens import UserTokens
+
 
 class TestUser(unittest.TestCase):
 
-    def  test_slack(self):
+    def test_slack(self):
         user = UserTokens()
         user.slack = "foobar"
         self.assertEqual(user.slack, "foobar")
 
-    def  test_github(self):
+    def test_github(self):
         user = UserTokens()
         user.github = "fizzbuzz"
         self.assertEqual(user.github, "fizzbuzz")
 
-    def  test_stackoverflow(self):
+    def test_stackoverflow(self):
         user = UserTokens()
         user.stackoverflow = "overflow"
         self.assertEqual(user.stackoverflow, "overflow")
 
-    def  test_trello(self):
+    def test_trello(self):
         user = UserTokens()
         user.trello = "trello"
         self.assertEqual(user.trello, "trello")
+
 
 if __name__ == "__main__":
     unittest.main()
