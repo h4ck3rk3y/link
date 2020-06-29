4fccb13994d166483f2315f1c7c9faab753ed2cd
Sodhi <prabhjyotsingh95@gmail.com>
2020-06-29 22:39:17 +0100

    adding a setup.py for this to be importable in fackend"

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='link',
+    packages=['link'],
+    description="Link - Fetch's link to the rest of world",
+    version='0.1',
+    url='https://github.com/h4ck3rk3y/link',
+    author='Fetch',
+    author_email='hello@gofetch.io',
+    keywords=[]
+    )
