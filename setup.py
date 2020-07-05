1e15379fad5b329864bd218628c0238f1215900a
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-06 00:22:05 +0400

    fix packages and sub packages (#13)
    * fix packages and sub packages
    
    * new version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='link',
-    packages=['link'],
+    packages=setuptools.find_packages()
     description="Link - Fetch's link to the rest of world",
-    version='0.1',
+    version='0.2',
     url='https://github.com/h4ck3rk3y/link',
     author='Fetch',
     author_email='hello@gofetch.io',
