d7384531386d7220e80c0a1ded98bd3c723f3ef6
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-06 00:26:03 +0400

    added a comma

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='link',
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
     description="Link - Fetch's link to the rest of world",
     version='0.2',
     url='https://github.com/h4ck3rk3y/link',
