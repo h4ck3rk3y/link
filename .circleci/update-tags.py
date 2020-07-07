0f6ad3c3d55cbea278a479228e5a308ace1111d5
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-07 23:52:47 +0400

    this should take care of circle

---
+++ .circleci/update-tags.py
@@ -0,0 +1,25 @@
+import re
+import os
+
+def get_version():
+	with open("setup.py", "r") as setupfile:
+		contents = setupfile.read()
+		regex = r"version=('(\d*)\.(\d*)\.(\d*)')"
+		result = re.search(regex, contents)
+		whole = result.group(1)
+		major = result.group(2)
+		minor = result.group(3)
+		patch = result.group(4)
+		return whole, major, minor, patch
+
+
+def delete_and_publish(tag):
+	os.system(f"git tag -d {tag}")
+	os.system(f"git push oriign :refs/tags/{tag}")
+	os.system(f"git tag {tag}")
+	os.system(f"git push --tags")
+
+def main():
+	whole, major, minor, patch = get_version()
+	delete_and_publish(whole)
+	delete_and_publish(f"{major}.x")
