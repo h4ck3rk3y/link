6f11f70b074c28e84fb428382f83c4ef5f0ab7b1
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-08 00:25:59 +0400

    circle automate tags (#16)
    * this should take care of circle
    
    * added jobs
    
    * this should fix circle
    
    * test
    
    * maybe version 2 works
    
    * in the blind here
    
    * brain fart avoided
    
    * fix circle
    
    * i should timebox this exercise
    
    * i hope thisi is it
    
    * added something to execute
    
    * fixed delete and publish

---
+++ .circleci/update-tags.py
@@ -0,0 +1,28 @@
+import re
+import os
+
+def get_version():
+	with open("setup.py", "r") as setupfile:
+		contents = setupfile.read()
+		regex = r"version=('(\d*)\.(\d*)\.(\d*)')"
+		result = re.search(regex, contents)
+		whole = result.group(1)
+		major = result.group(2)
+		minor = result.group(3)
+		patch = result.group(4)
+		return whole, major, minor, patch
+
+
+def delete_and_publish(tag):
+	os.system(f"git tag -d {tag} || true")
+	os.system(f"git push origin :refs/tags/{tag}")
+	os.system(f"git tag {tag}")
+	os.system(f"git push origin {tag}")
+
+def main():
+	whole, major, minor, patch = get_version()
+	delete_and_publish(whole)
+	delete_and_publish(f"{major}.x")
+
+if __name__ == "__main__":
+	main()
