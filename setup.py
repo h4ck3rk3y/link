0a9c1cf1ada2412eab4354445851226a238255ae
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-08 22:32:21 +0400

    patch version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='link',
     packages=setuptools.find_packages(),
     description="Link - Fetch's link to the rest of world",
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/h4ck3rk3y/link',
     author='Fetch',
     author_email='hello@gofetch.io',
