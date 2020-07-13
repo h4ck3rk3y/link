fb19b8494ed5cb3c819181204d0ea00b1d3e35c5
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-13 22:59:52 +0400

    added a test warmer

--- tests/test_link.py
+++ tests/test_link.py
@@ -6,6 +6,23 @@ from datetime import datetime
 
 class TestLink(unittest.TestCase):
 
+    def test_warmup_the_request(self):
+        user_token = UserTokens(stackoverflow=UserToken(
+            token=""), github=UserToken(token=""))
+        link = Link.builder(user_token).query("python").page_size(12)
+        now = datetime.now()
+        result = link.fetch()
+        later = datetime.now()
+        time_elapsed = later - now
+        time_elapsed = time_elapsed.total_seconds()
+        i = 2.0
+        self.assertEqual(len(result), 12)
+        while True:
+            if i < 0.0625:
+                break
+            self.assertLessEqual(time_elapsed, i)
+            i = i/2.0
+
     def test_both_github_and_stackoverflow(self):
 
         user_token = UserTokens(stackoverflow=UserToken(
