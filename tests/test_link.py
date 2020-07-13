57d9fae11127dff426971e60bdece903b1dfa1bc
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-13 23:09:27 +0400

    added a token request

--- tests/test_link.py
+++ tests/test_link.py
@@ -2,9 +2,24 @@ from link.core import Link
 from link.models.user_tokens import UserTokens, UserToken
 import unittest
 from datetime import datetime
+import requests
 
 
 class TestLink(unittest.TestCase):
+
+    def test_a_token_request_that_runs_before_everyone(self):
+        now = datetime.now()
+        result = requests.get("https://api.github.com")
+        later = datetime.now()
+        time_elapsed = later - now
+        time_elapsed = time_elapsed.total_seconds()
+        i = 2.0
+        self.assertEqual(len(result), 12)
+        while True:
+            if i < 0.0625:
+                break
+            self.assertLessEqual(time_elapsed, i)
+            i = i/2.0
 
     def test_warmup_the_request(self):
         user_token = UserTokens(stackoverflow=UserToken(
