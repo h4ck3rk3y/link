ab10da9082c32f12b4d42dbbc77f165dd95edd11
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-13 23:29:11 +0400

    lets send a couple of token requests

--- tests/test_link.py
+++ tests/test_link.py
@@ -19,6 +19,8 @@ class TestLink(unittest.TestCase):
         later = datetime.now()
         time_elapsed = later - now
         time_elapsed = time_elapsed.total_seconds()
+        requests.get(
+            "https://api.github.com/search/issues", headers=HEADERS, params={"q": "python", "per_page": 12})
         i = 2.0
         self.assertEqual(len(data['items']), 12)
         while True:
