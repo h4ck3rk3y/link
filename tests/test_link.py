4d266132bd62117dd950924bdc835bb91e09171c
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-15 23:54:42 +0400

    ordering

--- tests/test_link.py
+++ tests/test_link.py
@@ -13,21 +13,7 @@ HEADERS = {
 
 class TestLink(unittest.TestCase):
 
-    def setUp(self):
-        """init each test"""
-        self.pr = cProfile.Profile()
-        self.pr.enable()
-        print("\n<<<---")
-
-    def tearDown(self):
-        """finish any test"""
-        p = Stats(self.pr)
-        p.strip_dirs()
-        p.sort_stats('tottime')
-        p.print_stats()
-        print("\n--->>>")
-
-    def test_fire_forget(self):
+    def test_1_python(self):
         user_token = UserTokens(stackoverflow=UserToken(
             token=""), github=UserToken(token=""))
         link = Link.builder(user_token).query("python").page_size(12)
@@ -44,7 +30,7 @@ class TestLink(unittest.TestCase):
             if i < 0.0625:
                 break
 
-    def test_fire_forget_round_two(self):
+    def test_2_golang(self):
         user_token = UserTokens(stackoverflow=UserToken(
             token=""), github=UserToken(token=""))
         link = Link.builder(user_token).query("golang").page_size(12)
@@ -63,10 +49,48 @@ class TestLink(unittest.TestCase):
             if i < 0.0625:
                 break
 
-    def test_fire_forget_terminal(self):
+    def test_3_python(self):
         user_token = UserTokens(stackoverflow=UserToken(
             token=""), github=UserToken(token=""))
         link = Link.builder(user_token).query("python").page_size(12)
+        now = datetime.now()
+        result = link.fetch()
+        later = datetime.now()
+        time_elapsed = later - now
+        time_elapsed = time_elapsed.total_seconds()
+        i = 2.0
+        self.assertEqual(len(result), 12)
+        i = 2.0
+        self.assertEqual(len(result), 12)
+        while i:
+            self.assertLessEqual(time_elapsed, i)
+            i = i / 2.0
+            if i < 0.0625:
+                break
+
+    def test_4_golang(self):
+        user_token = UserTokens(stackoverflow=UserToken(
+            token=""), github=UserToken(token=""))
+        link = Link.builder(user_token).query("golang").page_size(12)
+        now = datetime.now()
+        result = link.fetch()
+        later = datetime.now()
+        time_elapsed = later - now
+        time_elapsed = time_elapsed.total_seconds()
+        i = 2.0
+        self.assertEqual(len(result), 12)
+        i = 2.0
+        self.assertEqual(len(result), 12)
+        while i:
+            self.assertLessEqual(time_elapsed, i)
+            i = i / 2.0
+            if i < 0.0625:
+                break
+
+    def test_5_java(self):
+        user_token = UserTokens(stackoverflow=UserToken(
+            token=""), github=UserToken(token=""))
+        link = Link.builder(user_token).query("java").page_size(12)
         now = datetime.now()
         result = link.fetch()
         later = datetime.now()
