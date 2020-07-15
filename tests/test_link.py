049737d55e5573c64aa4e428e9401dee13abdb40
Gyanendra Mishra <anomaly.the@gmail.com>
2020-07-15 23:43:14 +0400

    just send the same request multipel times

--- tests/test_link.py
+++ tests/test_link.py
@@ -27,23 +27,6 @@ class TestLink(unittest.TestCase):
         p.print_stats()
         print("\n--->>>")
 
-    def test_a_token_request_that_runs_before_everyone(self):
-        for query in ["python", "java", "golang", "c++", "ocaml"]:
-            now = datetime.now()
-            response = requests.get(
-                "https://api.github.com/search/issues", headers=HEADERS, params={"q": query, "per_page": 12})
-            data = response.json()
-            later = datetime.now()
-            time_elapsed = later - now
-            time_elapsed = time_elapsed.total_seconds()
-            self.assertEqual(len(data['items']), 12)
-            i = 2.0
-            while True:
-                if i < 0.0625:
-                    break
-                self.assertLessEqual(time_elapsed, i)
-                i = i/2.0
-
     def test_fire_forget(self):
         user_token = UserTokens(stackoverflow=UserToken(
             token=""), github=UserToken(token=""))
@@ -56,8 +39,7 @@ class TestLink(unittest.TestCase):
         i = 2.0
         self.assertEqual(len(result), 12)
 
-    def test_yolo_both_github_and_stackoverflow(self):
-
+    def test_fire_forget_round_two(self):
         user_token = UserTokens(stackoverflow=UserToken(
             token=""), github=UserToken(token=""))
         link = Link.builder(user_token).query("python").page_size(12)
@@ -68,33 +50,10 @@ class TestLink(unittest.TestCase):
         time_elapsed = time_elapsed.total_seconds()
         i = 2.0
         self.assertEqual(len(result), 12)
-        while True:
-            if i < 0.0625:
-                break
-            self.assertLessEqual(time_elapsed, i)
-            i = i/2.0
 
-    def test_just_github(self):
-
-        user_token = UserTokens(github=UserToken(token=""))
-        link = Link.builder(user_token).query("python").page_size(12)
-        now = datetime.now()
-        result = link.fetch()
-        later = datetime.now()
-        time_elapsed = later - now
-        time_elapsed = time_elapsed.total_seconds()
-        i = 2.0
-        self.assertEqual(len(result), 12)
-        while True:
-            if i < 0.03125:
-                break
-            self.assertLessEqual(time_elapsed, i)
-            i = i/2.0
-
-    def test_just_stackoverflow(self):
-
+    def test_fire_forget_terminal(self):
         user_token = UserTokens(stackoverflow=UserToken(
-            token=""))
+            token=""), github=UserToken(token=""))
         link = Link.builder(user_token).query("python").page_size(12)
         now = datetime.now()
         result = link.fetch()
@@ -103,24 +62,3 @@ class TestLink(unittest.TestCase):
         time_elapsed = time_elapsed.total_seconds()
         i = 2.0
         self.assertEqual(len(result), 12)
-        while True:
-            if i < 0.0625:
-                break
-            self.assertLessEqual(time_elapsed, i)
-            i = i/2.0
-
-    def test_yz_token_request_that_runs_before_everyone(self):
-        now = datetime.now()
-        response = requests.get(
-            "https://api.github.com/search/issues", headers=HEADERS, params={"q": "python", "per_page": 12})
-        data = response.json()
-        later = datetime.now()
-        time_elapsed = later - now
-        time_elapsed = time_elapsed.total_seconds()
-        i = 2.0
-        self.assertEqual(len(data['items']), 12)
-        while True:
-            if i < 0.0625:
-                break
-            self.assertLessEqual(time_elapsed, i)
-            i = i/2.0
