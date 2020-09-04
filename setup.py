71c97d22ea45f69dafd754ba0ac9d9f605d07f78
Gyanendra Mishra <anomaly.the@gmail.com>
2020-09-04 23:23:25 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='link',
     packages=setuptools.find_packages(),
     description="Link - Fetch's link to the rest of world",
-    version='2.7.8',
+    version='2.7.0',
     url='https://github.com/h4ck3rk3y/link',
     author='Fetch',
     author_email='hello@gofetch.io',
