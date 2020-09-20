32b7acdf5f59ae01635f8407fb2c367f1b87f2c9
Gyanendra Mishra <anomaly.the@gmail.com>
2020-09-20 13:14:34 +0400

    separate gitlab issues

---
+++ link/searchers/link_gitlab_issues.py
@@ -0,0 +1,29 @@
+from .gitlab import Gitlab
+import grequests
+from ..models.results import SingleResult, Page
+from datetime import datetime
+from .constants import ISSUE
+from datetime import timedelta
+
+
+class Searcher(Gitlab):
+
+    source = "gitlab"
+    name = "gitlab_issues"
+
+    def __init__(self, token, username, query, per_page, source_result):
+        super().__init__(token, username, query, per_page,
+                         source_result, Searcher.name, "issues")
+
+    def parse(self, response):
+        result_page = Page()
+        for item in response['items']:
+            link = item["web_url"]
+            preview = item["description"]
+            title = item["title"]
+            created_at = datetime.strptime(
+                item["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            single_result = SingleResult(
+                preview=preview, link=link, source=Searcher.source, date=created_at, category=ISSUE, title=title)
+            result_page.add(single_result)
+        return result_page
