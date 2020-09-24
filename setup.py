fbf1cd7c9352f21d85714da213ff56ff5b29c238
Prabhjyot Singh Sodhi <prabhjyotsingh95@gmail.com>
2020-09-24 23:40:51 +0530

    Add install requires section for proper installation of transitive dependencies (#50)

--- setup.py
+++ setup.py
@@ -8,5 +8,9 @@ setuptools.setup(
     url='https://github.com/h4ck3rk3y/link',
     author='Fetch',
     author_email='hello@gofetch.io',
-    keywords=[]
+    keywords=[],
+    install_requires=[
+        "grequests>=0.6.0",
+        "requests>=2.24.0",
+    ],
 )
